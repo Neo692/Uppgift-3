0c10d85b581cfa096cd0c6091696269d7a4802b3
nehy06001 <nehy06001@E-PW05CGQV>
2024-09-12 15:49:16 +0200

    Add project files.

---
+++ Uppgift 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uppgift_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /\"\\\\\"\\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
